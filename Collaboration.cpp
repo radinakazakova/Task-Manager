c1ee79260cc09c7c8635398d9908938026e25dac
radinakazakova <147272490+radinakazakova@users.noreply.github.com>
2024-06-29 00:26:00 +0300

    updated serialize/deserialize

--- Collaboration.cpp
+++ Collaboration.cpp
@@ -1,6 +1,7 @@
 #include "Collaboration.h"
 
 int Collaboration::uniqueId = -1;
+static constexpr int MAX_LENGTH = 100;
 
 Collaboration::Collaboration()
 {
@@ -8,15 +9,13 @@ Collaboration::Collaboration()
 	id = uniqueId;
 }
 
-Collaboration::Collaboration(int index, const MyString& name, User* creator): index(index), name(name), creator(creator)
+Collaboration::Collaboration(int index, const MyString& name, User* creator) : index(index), name(name), creator(creator)
 {
 	++uniqueId;
 	id = uniqueId;
 	users.addAtFirstFreeIndex(*creator);
-	
+
 }
-
-
 
 const MyString& Collaboration::getName() const
 {
@@ -44,9 +43,19 @@ size_t Collaboration::getTasksCount() const
 	return tasks.getSize();
 }
 
+int Collaboration::getId() const
+{
+	return id;
+}
+
 void Collaboration::setCreator(User* creator)
 {
 	this->creator = creator;
+}
+
+void Collaboration::setInvalidId()
+{
+	id = -1;
 }
 
 void Collaboration::serialize(std::ofstream& ofs) const
@@ -54,30 +63,34 @@ void Collaboration::serialize(std::ofstream& ofs) const
 	ofs.write(reinterpret_cast<const char*>(&index), sizeof(index));
 	ofs.write(reinterpret_cast<const char*>(&id), sizeof(id));
 
-	size_t nameSize = name.getLength();
-	ofs.write(reinterpret_cast<const char*>(&nameSize), sizeof(nameSize));
-	ofs.write(name.c_str(), nameSize);
-
-	int indexCreator = creator->getIndex();
-	ofs.write(reinterpret_cast<const char*>(&indexCreator), sizeof(indexCreator));
-
-	size_t tasksSize = tasks.getSize();
-	ofs.write(reinterpret_cast<const char*>(&tasksSize), sizeof(tasksSize));
-	for(size_t i = 0; i < tasksSize; ++i)
+	if (id != -1)
 	{
-		int first = tasks[i].getFirst()->getIndex();
-		int second = tasks[i].getSecond()->getIndex();
-		ofs.write(reinterpret_cast<const char*>(&first), sizeof(first));
-		ofs.write(reinterpret_cast<const char*>(&second), sizeof(second));
+		int nameSize = name.getLength();
+		ofs.write(reinterpret_cast<const char*>(&nameSize), sizeof(nameSize));
+		ofs.write(name.c_str(), nameSize);
+
+		int indexCreator = creator->getIndex();
+		ofs.write(reinterpret_cast<const char*>(&indexCreator), sizeof(indexCreator));
+
+		int tasksSize = tasks.getSize();
+		ofs.write(reinterpret_cast<const char*>(&tasksSize), sizeof(tasksSize));
+		for (int i = 0; i < tasksSize; ++i)
+		{
+			int first = tasks[i].getFirst()->getIndex();
+			int second = tasks[i].getSecond()->getIndex();
+			ofs.write(reinterpret_cast<const char*>(&first), sizeof(first));
+			ofs.write(reinterpret_cast<const char*>(&second), sizeof(second));
+		}
+
+		int usersSize = users.getSize();
+		ofs.write(reinterpret_cast<const char*>(&usersSize), sizeof(usersSize));
+		for (int i = 0; i < usersSize; i++)
+		{
+			int currUserIndex = users[i].getIndex();
+			ofs.write(reinterpret_cast<const char*>(&currUserIndex), sizeof(currUserIndex));
+		}
 	}
 
-	size_t usersSize = users.getSize();
-	ofs.write(reinterpret_cast<const char*>(&usersSize), sizeof(usersSize));
-	for(int i = 0; i < usersSize; i++)
-	{
-		int currUserIndex = users[i].getIndex();
-		ofs.write(reinterpret_cast<const char*>(&currUserIndex), sizeof(currUserIndex));
-	}
 }
 
 void Collaboration::deserialize(std::ifstream& ifs)
@@ -85,11 +98,15 @@ void Collaboration::deserialize(std::ifstream& ifs)
 	ifs.read(reinterpret_cast<char*>(&index), sizeof(index));
 	ifs.read(reinterpret_cast<char*>(&id), sizeof(id));
 
-	size_t nameSize;
-	ifs.read(reinterpret_cast<char*>(&nameSize), sizeof(nameSize));
-	MyString tempName(nameSize);
-	ifs.read(&tempName[0], nameSize);
-	name = tempName;
+	if (id != -1)
+	{
+		int nameSize;
+		ifs.read(reinterpret_cast<char*>(&nameSize), sizeof(nameSize));
+		char tempName[MAX_LENGTH] = {};
+		ifs.read(tempName, nameSize);
+		tempName[nameSize] = '\0';
+		name = tempName;
+	}
 
 }
 
@@ -98,9 +115,9 @@ bool Collaboration::userIsInCollaboration(User& user) const
 {
 	int size = users.getSize();
 
-	for(int i = 0; i < size; i++)
+	for (int i = 0; i < size; i++)
 	{
-		if(&users[i] == &user)
+		if (&users[i] == &user)
 		{
 			return true;
 		}
@@ -113,11 +130,11 @@ void Collaboration::deleteCollaboration()
 {
 	int size = users.getSize();
 
-	for(int i = 0; i < size; i++)
+	for (int i = 0; i < size; i++)
 	{
 		int tasksCount = tasks.getSize();
 
-		for(int j = 0 ; j < tasksCount; j++)
+		for (int j = 0; j < tasksCount; j++)
 		{
 			users[i].deleteTask(tasks[j].getFirst()->getId());
 		}
@@ -140,7 +157,7 @@ void Collaboration::addUser(User& userToAdd)
 
 	int tasksCount = tasks.getSize();
 
-	for(int i = 0; i < tasksCount; i++)
+	for (int i = 0; i < tasksCount; i++)
 	{
 		userToAdd.addTask(*tasks[i].getFirst());
 	}
@@ -152,7 +169,7 @@ void Collaboration::addTask(User& assignee, Task& taskToAdd)
 
 	int size = users.getSize();
 
-	for(int i = 0; i < size; i++)
+	for (int i = 0; i < size; i++)
 	{
 		users[i].addTask(taskToAdd);
 	}
@@ -162,10 +179,13 @@ void Collaboration::listTasks() const
 {
 	int size = tasks.getSize();
 
-	for(int i = 0; i < size; i++)
+	for (int i = 0; i < size; i++)
 	{
-		tasks[i].getFirst()->printTask();
-		std::cout << "Assignee: " << tasks[i].getSecond()->getUsername() << std::endl;
-		std::cout << "\n";
+		if (tasks[i].getFirst()->getId() != -1)
+		{
+			tasks[i].getFirst()->printTask();
+			std::cout << "Assignee: " << tasks[i].getSecond()->getUsername() << std::endl;
+			std::cout << "\n";
+		}
 	}
 }
