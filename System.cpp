094bfd60c1a7d7570f2151642d46b344a3adba1a
radinakazakova <147272490+radinakazakova@users.noreply.github.com>
2024-06-28 18:43:40 +0300

    updated using SharedPtr

--- System.cpp
+++ System.cpp
@@ -8,7 +8,7 @@ int System::findUserIdx(const MyString& username) const
 
 	for (int i = 0; i < size; i++)
 	{
-		if (allUsers[i].getUsername() == username)
+		if (allUsers[i]->getUsername() == username)
 		{
 			return i;
 		}
@@ -19,11 +19,16 @@ int System::findUserIdx(const MyString& username) const
 
 int System::findTask(int id) const
 {
+	if(id == -1)
+	{
+		return -1;
+	}
+
 	int tasksCount = allTasks.getSize();
 
 	for (int i = 0; i < tasksCount; i++)
 	{
-		if (allTasks[i].getId() == id)
+		if (allTasks[i]->getId() == id)
 		{
 			return i;
 		}
@@ -36,9 +41,9 @@ int System::findCollaboration(const MyString& name) const
 {
 	int size = allCollaborations.getSize();
 
-	for (int i = 0; i< size; i++)
+	for (int i = 0; i < size; i++)
 	{
-		if(allCollaborations[i].getName() == name)
+		if (allCollaborations[i]->getId() != -1 && allCollaborations[i]->getName() == name)
 		{
 			return i;
 		}
@@ -50,9 +55,15 @@ int System::findCollaboration(const MyString& name) const
 void System::deleteTasksFromCollaboration(Collaboration& collaboration)
 {
 	int size = collaboration.getTasksCount();
-	for(int i = 0; i < size; i++)
+
+	for (int i = 0; i < size; i++)
 	{
-		allTasks.popAt(findTask(collaboration.getTaskFromIndex(i)->getId()));
+		int index = findTask(collaboration.getTaskFromIndex(i)->getId());
+
+		if (index != -1)
+		{
+			allTasks[index]->setInvalidId();
+		}
 	}
 }
 
@@ -60,7 +71,7 @@ void System::saveDataToFile() const
 {
 	std::ofstream ofs(filePath, std::ios::binary);
 
-	if(!ofs.is_open())
+	if (!ofs.is_open())
 	{
 		throw std::exception("Cannot open file");
 	}
@@ -73,17 +84,17 @@ void System::saveDataToFile(std::ofstream& ofs) const
 	int tasksCount = allTasks.getSize();
 	ofs.write(reinterpret_cast<const char*>(tasksCount), sizeof(tasksCount));
 
-	for(int i = 0; i < tasksCount; i++)
+	for (int i = 0; i < tasksCount; i++)
 	{
-		allTasks[i].serialize(ofs);
+		allTasks[i]->serialize(ofs);
 	}
 
 	int usersCount = allUsers.getSize();
 	ofs.write(reinterpret_cast<const char*>(&usersCount), sizeof(usersCount));
 
-	for(int i = 0; i < usersCount; i++)
+	for (int i = 0; i < usersCount; i++)
 	{
-		allUsers[i].serialize(ofs);
+		allUsers[i]->serialize(ofs);
 	}
 
 	int collabsCount = allCollaborations.getSize();
@@ -91,7 +102,7 @@ void System::saveDataToFile(std::ofstream& ofs) const
 
 	for (int i = 0; i < collabsCount; i++)
 	{
-		allCollaborations[i].serialize(ofs);
+		allCollaborations[i]->serialize(ofs);
 	}
 }
 
@@ -100,20 +111,20 @@ void System::loadDataFromFile(std::ifstream& ifs)
 	int tasksCount;
 
 	if (ifs.read(reinterpret_cast<char*>(&tasksCount), sizeof(tasksCount))) {
-		Vector<Task> readTasks(tasksCount);
+		Vector<SharedPtr<Task>> readTasks(tasksCount);
 		for (int i = 0; i < tasksCount; i++)
 		{
-			readTasks[i].deserialize(ifs);
+			readTasks[i]->deserialize(ifs);
 		}
 		allTasks = readTasks;
 
 		int usersCount;
 		ifs.read(reinterpret_cast<char*>(&usersCount), sizeof(usersCount));
 
-		Vector<User> readUsers(usersCount);
+		Vector<SharedPtr<User>> readUsers(usersCount);
 		for (int i = 0; i < usersCount; i++)
 		{
-			readUsers[i].deserialize(ifs);
+			readUsers[i]->deserialize(ifs);
 
 			int tasksIndexesCount;
 			ifs.read(reinterpret_cast<char*>(&tasksIndexesCount), sizeof(tasksIndexesCount));
@@ -121,7 +132,7 @@ void System::loadDataFromFile(std::ifstream& ifs)
 			{
 				int currIndex;
 				ifs.read(reinterpret_cast<char*>(&currIndex), sizeof(currIndex));
-				readUsers[i].addTask(allTasks[currIndex]);
+				readUsers[i]->addTask(*allTasks[currIndex]);
 			}
 		}
 		allUsers = readUsers;
@@ -129,15 +140,15 @@ void System::loadDataFromFile(std::ifstream& ifs)
 		int collabsCount;
 		ifs.read(reinterpret_cast<char*>(&collabsCount), sizeof(collabsCount));
 
-		Vector<Collaboration> readCollaborations(collabsCount);
+		Vector<SharedPtr<Collaboration>> readCollaborations(collabsCount);
 		for (int i = 0; i < collabsCount; i++)
 		{
-			readCollaborations[i].deserialize(ifs);
+			readCollaborations[i]->deserialize(ifs);
 
 			int creatorIndex;
 			ifs.read(reinterpret_cast<char*>(&creatorIndex), sizeof(creatorIndex));
 
-			readCollaborations[i].setCreator(&allUsers[creatorIndex]);
+			readCollaborations[i]->setCreator(allUsers[creatorIndex].operator->());
 
 			int tasksCount;
 			ifs.read(reinterpret_cast<char*>(&tasksCount), sizeof(tasksCount));
@@ -149,7 +160,7 @@ void System::loadDataFromFile(std::ifstream& ifs)
 				ifs.read(reinterpret_cast<char*>(&currIndexTask), sizeof(currIndexTask));
 				ifs.read(reinterpret_cast<char*>(&currIndexAssignee), sizeof(currIndexAssignee));
 
-				readCollaborations[i].addDeserializedTask(allUsers[currIndexAssignee], allTasks[currIndexTask]);
+				readCollaborations[i]->addDeserializedTask(*allUsers[currIndexAssignee], *allTasks[currIndexTask]);
 			}
 
 			int usersCount;
@@ -159,7 +170,7 @@ void System::loadDataFromFile(std::ifstream& ifs)
 			{
 				int currIndex;
 				ifs.read(reinterpret_cast<char*>(&currIndex), sizeof(currIndex));
-				readCollaborations[i].addDeserializedUser(allUsers[currIndex]);
+				readCollaborations[i]->addDeserializedUser(*allUsers[currIndex]);
 			}
 		}
 		allCollaborations = readCollaborations;
@@ -170,7 +181,7 @@ void System::loadDataFromFile()
 {
 	std::ifstream ifs(filePath, std::ios::binary);
 
-	if(!ifs.is_open())
+	if (!ifs.is_open())
 	{
 		throw std::exception("Couldn't open file");
 	}
@@ -181,7 +192,7 @@ void System::loadDataFromFile()
 
 void System::registerUser(const MyString& username, const MyString& password)
 {
-	if(password.getLength() == 0 || username.getLength() == 0)
+	if (password.getLength() == 0 || username.getLength() == 0)
 	{
 		std::cout << "Please input password and/or username." << std::endl;
 	}
@@ -189,7 +200,7 @@ void System::registerUser(const MyString& username, const MyString& password)
 	if (findUserIdx(username) == -1)
 	{
 		int userIndex = allUsers.getSize();
-		allUsers.pushBack(User(userIndex, username, password));
+		allUsers.pushBack(SharedPtr<User>( new User(userIndex, username, password)));
 
 		std::cout << "Successfully created account!" << std::endl;
 	}
@@ -203,17 +214,17 @@ void System::loginUser(const MyString& username, const MyString& password)
 {
 	int userIdx = findUserIdx(username);
 
-	if (userIdx != -1 && allUsers[userIdx].checkPassword(password))
+	if (userIdx != -1 && allUsers[userIdx]->checkPassword(password))
 	{
 		std::time_t currTime = std::time(nullptr);
 
 		if (localtime_s(&loggedInTime, &currTime) != 0)
 		{
-			std::cerr << "Failed to get loggedInTime" << std::endl; 
+			std::cerr << "Failed to get loggedInTime" << std::endl;
 		}
-		else 
+		else
 		{
-			loggedInUser = &allUsers[userIdx];
+			loggedInUser = allUsers[userIdx].operator->();
 			loggedInUser->updateTaskStatus(loggedInTime);
 			loggedInUser->updateDashboard(loggedInTime);
 			std::cout << "Login successful" << std::endl;
@@ -227,7 +238,7 @@ void System::loginUser(const MyString& username, const MyString& password)
 
 void System::addTask(const MyString& name, const std::tm& due_date, const MyString& description)
 {
-	if(!loggedInUser)
+	if (!loggedInUser)
 	{
 		std::cout << "Please log in." << std::endl;
 		return;
@@ -240,9 +251,8 @@ void System::addTask(const MyString& name, const std::tm& due_date, const MyStri
 	else
 	{
 		int indexNewTask = allTasks.getSize();
-		allTasks.pushBack(Task(name, due_date, description, indexNewTask));
-	
-		loggedInUser->addTask(allTasks[indexNewTask]);
+		allTasks.pushBack(SharedPtr<Task>(new Task(name, due_date, description, indexNewTask)));
+		loggedInUser->addTask(*allTasks[indexNewTask]);
 		std::cout << "Task added successfully!" << std::endl;
 	}
 }
@@ -262,9 +272,9 @@ void System::addTask(const MyString& name, const MyString& description)
 	else
 	{
 		int indexNewTask = allTasks.getSize();
-		allTasks.pushBack(Task(name, description, indexNewTask));
+		allTasks.pushBack(SharedPtr<Task>(new Task(name, description, indexNewTask)));
 
-		loggedInUser->addTask(allTasks[indexNewTask]);
+		loggedInUser->addTask(*allTasks[indexNewTask]);
 		std::cout << "Task added successfully!" << std::endl;
 
 	}
@@ -333,13 +343,13 @@ void System::deleteTask(int id)
 		return;
 	}
 
-	if(loggedInUser->deleteTask(id))
+	if (loggedInUser->deleteTask(id))
 	{
 		int index = findTask(id);
 
-		if(index != -1)
+		if (index != -1)
 		{
-			allTasks.popAt(index);
+			allTasks[index]->setInvalidId();
 			std::cout << "Task deleted!" << std::endl;
 		}
 	}
@@ -417,17 +427,21 @@ void System::listDashboard() const
 
 void System::finishTask(int id)
 {
-	if (!loggedInUser)
+	if (id != -1)
 	{
-		std::cout << "Please log in." << std::endl;
-		return;
+		if (!loggedInUser)
+		{
+			std::cout << "Please log in." << std::endl;
+			return;
+		}
+
+		loggedInUser->finishTask(id);
+		loggedInUser->updateDashboard(loggedInTime);
 	}
 
-	loggedInUser->finishTask(id);
-	loggedInUser->updateDashboard(loggedInTime);
 }
 
-void System::logout()  
+void System::logout()
 {
 	if (!loggedInUser)
 	{
@@ -448,7 +462,7 @@ void System::addCollaboration(const MyString& name)
 	}
 
 	int index = allCollaborations.getSize();
-	allCollaborations.pushBack(Collaboration(index, name, loggedInUser));
+	allCollaborations.pushBack(SharedPtr<Collaboration>(new Collaboration(index, name, loggedInUser)));
 	std::cout << "Collaboration added successfully!" << std::endl;
 }
 
@@ -464,11 +478,11 @@ void System::deleteCollaboration(const MyString& name)
 
 	if (index != -1)
 	{
-		if (allCollaborations[index].getCreator() == loggedInUser)
+		if (allCollaborations[index]->getCreator() == loggedInUser)
 		{
-			allCollaborations[index].deleteCollaboration();
-			deleteTasksFromCollaboration(allCollaborations[index]);
-			allCollaborations.popAt(index);
+			allCollaborations[index]->deleteCollaboration();
+			deleteTasksFromCollaboration(*allCollaborations[index]);
+			allCollaborations[index]->setInvalidId();
 			std::cout << "Collaboration deleted!" << std::endl;
 		}
 		else
@@ -492,11 +506,11 @@ void System::listCollaborations() const
 
 	int size = allCollaborations.getSize();
 
-	for(int i = 0; i < size; i++)
+	for (int i = 0; i < size; i++)
 	{
-		const Collaboration& currCollab = allCollaborations[i];
+		const Collaboration& currCollab = *allCollaborations[i];
 
-		if(currCollab.userIsInCollaboration(*loggedInUser))
+		if (currCollab.getId() != -1 && currCollab.userIsInCollaboration(*loggedInUser))
 		{
 			std::cout << currCollab.getName() << std::endl;
 		}
@@ -514,11 +528,11 @@ void System::addUser(const MyString& collaborationName, const MyString& username
 	int indexCollab = findCollaboration(collaborationName);
 	int indexUser = findUserIdx(username);
 
-	if(indexCollab != -1 && indexUser != -1)
+	if (indexCollab != -1 && indexUser != -1)
 	{
-		if(allCollaborations[indexCollab].getCreator() == loggedInUser)
+		if (allCollaborations[indexCollab]->getCreator() == loggedInUser)
 		{
-			allCollaborations[indexCollab].addUser(allUsers[indexUser]);
+			allCollaborations[indexCollab]->addUser(*allUsers[indexUser]);
 			std::cout << "User added successfully!" << std::endl;
 		}
 		else
@@ -533,7 +547,7 @@ void System::addUser(const MyString& collaborationName, const MyString& username
 }
 
 void System::assignTask(const MyString& collaborationName, const MyString& username, const MyString& taskName,
-						const std::tm& due_date, const MyString& desc)
+	const std::tm& due_date, const MyString& desc)
 {
 	if (!loggedInUser)
 	{
@@ -544,13 +558,40 @@ void System::assignTask(const MyString& collaborationName, const MyString& usern
 	int collabIndex = findCollaboration(collaborationName);
 	int assigneeIndex = findUserIdx(username);
 
-	Collaboration& collaboration = allCollaborations[collabIndex];
-
-	if(collabIndex != -1 && assigneeIndex != -1 &&  collaboration.userIsInCollaboration(*loggedInUser)&& collaboration.userIsInCollaboration(allUsers[assigneeIndex]))
+	if (collabIndex != -1 && assigneeIndex != -1 && allCollaborations[collabIndex]->userIsInCollaboration(*loggedInUser) && allCollaborations[collabIndex]->userIsInCollaboration(*allUsers[assigneeIndex]))
 	{
+		Collaboration& collaboration = *allCollaborations[collabIndex];
+
 		int indexTask = allTasks.getSize();
-		allTasks.pushBack(Task(taskName, due_date, desc, indexTask));
-		collaboration.addTask(allUsers[assigneeIndex], allTasks[indexTask]);
+		allTasks.pushBack(SharedPtr<Task>(new Task(taskName, due_date, desc, indexTask)));
+		collaboration.addTask(*allUsers[assigneeIndex], *allTasks[indexTask]);
+		std::cout << "Task assigned successfully!" << std::endl;
+	}
+	else
+	{
+		std::cout << "Couldn't assign task!" << std::endl;
+	}
+}
+
+void System::assignTask(const MyString& collaborationName, const MyString& username, const MyString& taskName,
+	const MyString& desc)
+{
+	if (!loggedInUser)
+	{
+		std::cout << "Please log in." << std::endl;
+		return;
+	}
+
+	int collabIndex = findCollaboration(collaborationName);
+	int assigneeIndex = findUserIdx(username);
+
+	if (collabIndex != -1 && assigneeIndex != -1 && allCollaborations[collabIndex]->userIsInCollaboration(*loggedInUser) && allCollaborations[collabIndex]->userIsInCollaboration(*allUsers[assigneeIndex]))
+	{
+		Collaboration& collaboration = *allCollaborations[collabIndex];
+
+		int indexTask = allTasks.getSize();
+		allTasks.pushBack(SharedPtr<Task>(new Task(taskName, desc, indexTask)));
+		collaboration.addTask(*allUsers[assigneeIndex], *allTasks[indexTask]);
 		std::cout << "Task assigned successfully!" << std::endl;
 	}
 	else
@@ -569,9 +610,9 @@ void System::listTasks(const MyString& collaborationName) const
 
 	int index = findCollaboration(collaborationName);
 
-	if(index != -1 && allCollaborations[index].userIsInCollaboration(*loggedInUser))
+	if (index != -1 && allCollaborations[index]->userIsInCollaboration(*loggedInUser))
 	{
-		const Collaboration& collaboration = allCollaborations[index];
+		const Collaboration& collaboration = *allCollaborations[index];
 		collaboration.listTasks();
 	}
 	else
